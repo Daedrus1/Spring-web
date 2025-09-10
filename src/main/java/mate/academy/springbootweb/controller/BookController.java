579be0966eb2f12222d3ace44042a40430e75b58
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-09-10 22:39:28 +0200

    - Replaced manual field setters in BookServiceImpl.update() with BookMapper.updateEntityFromDto() - Renamed variable `current` to `existingBook` for clarity - Replaced deprecated @Where with @SQLRestriction in Book entity for soft delete

--- src/main/java/mate/academy/springbootweb/controller/BookController.java
+++ src/main/java/mate/academy/springbootweb/controller/BookController.java
@@ -6,15 +6,9 @@ import mate.academy.springbootweb.dto.BookDto;
 import mate.academy.springbootweb.dto.CreateBookRequestDto;
 import mate.academy.springbootweb.service.BookService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
 public class BookController {
